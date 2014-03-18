c497a179526711780d2f0eae02095f4c8e6b754f
Jimmy Tang <jcftang@gmail.com>
2014-03-18 21:16:47 +0000

    added env-setup.fish for fish useres

---
+++ hydra-stack/hacking/env-setup.fish
@@ -0,0 +1,33 @@
+#!/usr/bin/env fish
+# usage: . ./hacking/env-setup [-q]
+#    modifies environment for running Ansible from checkout
+set HACKING_DIR (dirname (status -f))
+set FULL_PATH (python -c "import os; print(os.path.realpath('$HACKING_DIR'))")
+set ANSIBLE_HOME (dirname $FULL_PATH)
+set -gx ANSIBLE_HOSTS $ANSIBLE_HOME/hosts.vagrant
+
+if set -q argv 
+    switch $argv
+    case '-q' '--quiet'
+    case '*'
+        echo ""
+        echo "Setting up Ansible to run out of checkout, hosts.vagrant and to use vagrant private keys..."
+        echo ""
+        echo "ANSIBLE_HOME=$ANSIBLE_HOME"
+        echo "ANSIBLE_HOSTS=$ANSIBLE_HOSTS"
+        echo ""
+
+        echo "Remember, you may wish to specify your host file with -i"
+        echo ""
+        echo "Done!"
+        echo ""
+   end
+end
+
+function ansible -d "ansible Wrapper"
+    /usr/bin/env ansible --user=vagrant --private-key="~/.vagrant.d/insecure_private_key" $argv
+end
+
+function ansible-playbook -d "ansible Wrapper"
+    /usr/bin/env ansible --user=vagrant --private-key="~/.vagrant.d/insecure_private_key" $argv
+end
